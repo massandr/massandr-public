86a0fce889192508fdd49156e691d02c4cb7d0a0
Andrey Massalskiy <47332847+massandr@users.noreply.github.com>
2025-06-26 23:04:23 -0400

    Update WN10-AU-000500.ps1
    template

--- STIGs/WN10-AU-000500.ps1
+++ STIGs/WN10-AU-000500.ps1
@@ -1,1 +1,27 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Andrey Massalskiy
+    LinkedIn        : linkedin.com/in/massandr/
+    GitHub          : github.com/massandr
+    Date Created    : 2025-06-26
+    Last Modified   : 2025-06-26
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AU-000500.ps1 
+#>
+
 
